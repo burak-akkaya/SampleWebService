b7dbe2107fc5251f56ce7cfa145e4dc5c8d2cdd7
Burak Akkaya <contact@burakakkaya.me>
2022-02-23 21:52:29 +0300

    Initial commit

---
+++ src/main/java/com/brkakkaya/SampleWebService/SampleWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.brkakkaya.SampleWebService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brkakkaya/SampleWebService/SampleWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brkakkaya.SampleWebService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
